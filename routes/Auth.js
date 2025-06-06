e2d0d6640aa6e1dd2155c4e8e3a148fe3a35800e
saikiran dasari <saikiran.dasari04@gmail.com>
2025-06-06 10:17:09 +0530

    [04-06-2025] Kiran | Updated auth

--- routes/Auth.js
+++ routes/Auth.js
@@ -7,6 +7,13 @@ const {
     adminLogin,
 } = require('../controllers/Auth');
 
+router.get('/test', (req,res) => {
+    try {
+        res.status(200).send({message : 'Server running...'})
+    }catch(error) {
+        res.status(500).send({message : "server not working...,",error})
+    }
+});
 router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.post('/admin/login', adminLogin);

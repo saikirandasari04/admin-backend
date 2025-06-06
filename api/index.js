e768320604104a0e763fab9bf578bc5fe098fa99
saikiran dasari <saikiran.dasari04@gmail.com>
2025-06-06 13:07:16 +0530

    [04-06-2025] Kiran | Updated package.json

--- api/index.js
+++ api/index.js
@@ -7,12 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({
-  origin: ['http://localhost:3000'], // add your allowed origins here
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-}));
-
 // MongoDB connection (reuse connection if already connected)
 const mongoUri = 'mongodb+srv://saikirandasari04:mRCCHvIkwSiRdU8x@chatapp.md9qa.mongodb.net';
 

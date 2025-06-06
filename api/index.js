d4ff5ac4ab587cd9f24628dc9092f583c70d3acb
saikiran dasari <saikiran.dasari04@gmail.com>
2025-06-06 09:54:41 +0530

    [04-06-2025] Kiran | Updated server less

---
+++ api/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection (reuse connection if already connected)
+const mongoUri = 'mongodb+srv://saikirandasari04:mRCCHvIkwSiRdU8x@chatapp.md9qa.mongodb.net';
+
+let isConnected = false;
+
+async function connectToDatabase() {
+  if (isConnected) {
+    return;
+  }
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  isConnected = true;
+}
+
+connectToDatabase().catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Import your route handlers
+const Auth = require("../routes/Auth");
+const Loan = require("../routes/Loan");
+
+// Use routes
+app.use(Auth);
+app.use(Loan);
+
+// Export the app wrapped with serverless-http
+module.exports = serverless(app);
